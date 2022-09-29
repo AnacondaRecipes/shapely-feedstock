86aad2ecf43fc445bf888305afda065b34139931
Serhii Kupriienko <skupriienko@anaconda.com>
2022-09-29 14:13:13 +0300

    Update to 1.8.4

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -1,13 +1,34 @@
-from shapely import speedups;
+import platform
+import py
+import os
+import sys
 
-assert speedups.available;
+implementation = platform.python_implementation()
+print('implementation: {}'.format(implementation))
+target_platform = os.environ["target_platform"]
+print(f'target platform: {target_platform}')
+py_version = sys.version_info[:2]
+print(f'python version: {py_version}')
 
-speedups.enable()
+pytest_args = ['tests']
+
+if implementation != 'PyPy':
+    from shapely import speedups
+    import shapely.speedups._speedups
+    import shapely.vectorized
+    import shapely.vectorized._vectorized
+
+    assert speedups.available;
+
+    speedups.enable()
+    pytest_args.append('--with-speedups')
+
+py.test.cmdline.main(pytest_args)
 
 from shapely.geometry import LineString
 
 ls = LineString([(0, 0), (10, 0)])
-# On OSX causes an abort trap, due to https://github.com/Toblerity/Shapely/issues/177
+# On OSX causes an abort trap, due to https://github.com/shapely/shapely/issues/177
 r = ls.wkt
 area = ls.buffer(10).area
 
